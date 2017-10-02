abf70da4147deb84ad6ebca139a35c9ae468f396
Natalia <natalia.kielbicka@o2.pl>
2017-10-02 19:53:40 +0200

    create new regex for amount input

--- src/components/views/ValidationAmountError.jsx
+++ src/components/views/ValidationAmountError.jsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 export default class ValidationAmountError extends Component {
     render() {
         return (
-            <span className="form__error">The amount should be a number.</span>
+            <span className="form__error">The amount should be an integer number.</span>
         )
     }
 }
